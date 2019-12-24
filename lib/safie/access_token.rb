4d85ad93fc701c4617368a662fcd3ee61679d6f2
nov <nov@matake.jp>
2019-12-24 12:46:32 +0900

    safie userinfo

--- lib/safie/access_token.rb
+++ lib/safie/access_token.rb
@@ -6,8 +6,36 @@ module Safie
       super attributes.merge(access_token: access_token)
     end
 
-    def token_info!
-      get File.join(ISSUER, '/auth/tokeninfo')
+    def token_info!(params = {})
+      resource_request do
+        get File.join(ISSUER, '/auth/tokeninfo'), params
+      end
+    end
+
+    def user_info!(params = {})
+      token_info = token_info! params
+      {
+        sub: token_info[:user_id],
+        email: token_info[:mail_address]
+      }
+    end
+
+    private
+
+    def resource_request
+      res = yield
+      case res.status
+      when 200
+        JSON.parse(res.body).with_indifferent_access
+      when 400
+        raise BadRequest.new('API Access Faild', res)
+      when 401
+        raise Unauthorized.new('Access Token Invalid or Expired', res)
+      when 403
+        raise Forbidden.new('Insufficient Scope', res)
+      else
+        raise HttpError.new(res.status, 'Unknown HttpError', res)
+      end
     end
   end
 end
